c1ffe1d3985ec3cbcd283f72c5179551a6bf7a8a
Project-x109 <amanuelgirma109@gmail.com>
2024-02-09 21:38:38 +0300

    minors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,3 @@
-const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -17,8 +16,7 @@ app.use(
     })
 );
 app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
     session({
