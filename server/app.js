a56bb4a2ced5e0eca7174796c1fb1fc7e0e36d21
Project-x109 <amanuelgirma109@gmail.com>
2024-02-10 12:43:27 +0300

    minors

--- server/app.js
+++ server/app.js
@@ -7,52 +7,59 @@ const { logMiddleware, store } = require("./middlewares/logMiddleware.js");
 const csrf = require("csurf");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
-const user = require("./routes/userRoutes");
-const lawIssue = require("./routes/lawIssueRoutes");
+const userRoutes = require("./routes/userRoutes");
+const lawIssueRoutes = require("./routes/lawIssueRoutes");
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-  origin: ["http://localhost:3000", "https://law-front-cj39.vercel.app"],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://law-front-cj39.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use(session({
-  secret: 'ABCDEFGHSABSDBHJCS',
-  resave: true,
-  saveUninitialized: true,
-  store: store,
-  cookie: {
-    httpOnly: true,
-    sameSite: 'none',
-    secure: true,
-    maxAge: 1000 * 60 * 60 * 24,
-  },
-}));
+app.use(
+  session({
+    secret: "ABCDEFGHSABSDBHJCS",
+    resave: true,
+    saveUninitialized: true,
+    store: store,
+    cookie: {
+      httpOnly: true,
+      sameSite: "lax",
+      maxAge: 1000 * 60 * 60 * 24,
+    },
+  })
+);
 
 // Create CSRF middleware
-const csrfProtection = csrf({ cookie: true, name: 'XSRF-TOKEN' });
+const csrfProtection = csrf({
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // CSRF token expires after 24 hours
+    secure: true, // CSRF token is only sent over HTTPS
+    httpOnly: true, // CSRF token is not accessible to client-side scripts
+  },
+  name: "XSRF-TOKEN",
+  value: (req) => req.csrfToken(),
+  failAction: (req, res) => {
+    res.status(403).send("Invalid CSRF token");
+  },
+});
 app.use(csrfProtection);
 
 // Set CSRF token in cookie and locals
 app.use((req, res, next) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.cookie("XSRF-TOKEN", req.csrfToken());
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
-// Logging CSRF Token
-app.use((req, res, next) => {
-  console.log("CSRF Token:", res.locals.csrfToken);
-  next();
-});
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", lawIssueRoutes);
 
-// CSRF token validation middleware
-app.use((err, req, res, next) => {
-  const error = []
-  if (err.code !== 'EBADCSRFTOKEN') return next(err);
-  error.push('Invalid CSRF token')
-  res.status(403).json({ success: false, error: error });
+app.get("/", (req, res) => {
+  res.send("Server is Running! 🚀");
 });
 
 if (process.env.NODE_ENV !== "production") {
@@ -62,18 +69,11 @@ if (process.env.NODE_ENV !== "production") {
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/get-csrf-token', (req, res) => {
+app.get("/get-csrf-token", (req, res) => {
   res.json({ csrfToken: req.csrfToken() });
 });
 
 app.use(logMiddleware);
-app.use("/api/v1", user);
-app.use("/api/v1", lawIssue);
-
-app.get("/", (req, res) => {
-  res.send("Server is Running! 🚀");
-});
-
 app.use(errorHandler);
 
 module.exports = app;
