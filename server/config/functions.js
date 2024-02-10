7dad7b21e6fa8c8102b39c84ebdefe63a7c378bc
Project-x109 <amanuelgirma109@gmail.com>
2024-02-10 14:15:21 +0300

    minors

--- server/config/functions.js
+++ server/config/functions.js
@@ -56,10 +56,6 @@ const isAuthenticated = (req, res, next) => {
   try {
     if (req.isAuthenticated()) {
       return next();
-    } else {
-      error.push('Unauthorized Error at is authenticated')
-      return res.status(401).json({ success: false, error: error });
-
     }
   } catch (err) {
     console.log(err)
