29a431822a58bdf2cd3ee5da24dfa32686d6e83c
Project-x109 <amanuelgirma109@gmail.com>
2024-02-10 22:04:28 +0300

    minors

--- server/routes/lawIssueRoutes.js
+++ server/routes/lawIssueRoutes.js
@@ -34,27 +34,27 @@ const {
     changeIssueStatusValidation,
     handleValidationErrors } = require("../middlewares/validation")
 
-router.route("/addissue").post(isAuthenticated, verifyToken, validateLawIssueData, handleValidationErrors, authorize(["admin", "employee"]), addIssue)
-router.route("/updateissue/:issueId").put(isAuthenticated, verifyToken, validateLawIssueData, handleValidationErrors, authorize(["admin", "employee"]), updateIssue);
-router.route("/getallissues").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getAllIssues);
-router.route("/addcomment/:issueId").post(isAuthenticated, verifyToken, validateLawIssueDataComment, handleValidationErrors, authorize(["admin", "employee"]), addComment);
-router.route("/getcomment/:issueId").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getComments);
-router.route("/updatecomment/:issueId/:commentId").put(isAuthenticated, verifyToken, validateLawIssueDataComment, handleValidationErrors, authorize(["admin", "employee"]), updateComment);
-router.route("/getissuebyid/:issueId").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getIssueById);
-router.route("/getissuesbycreatedby/:userId").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getIssuesByCreatedBy);
-router.route("/getissuesbylogin").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getIssuesByLogin);
-router.route("/deleteissue/:issueId").delete(isAuthenticated, verifyToken, authorize(["admin", "employee"]), deleteIssue);
-router.route("/deletecomment/:issueId/:commentId").delete(isAuthenticated, verifyToken, authorize(["admin", "employee"]), deleteComment);
-router.route("/exportlawissues").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), exportLawIssues);
-router.route("/getdashboardsummery").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getDashboardSummary);
-router.route("/getuserdashboardsummary").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getUserDashboardSummary);
-router.route("/changeissuestatus").put(isAuthenticated, verifyToken, changeIssueStatusValidation, handleValidationErrors, authorize(["admin", "employee"]), changeIssueStatus);
-router.route("/getrecentactivities").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getRecentActivities);
-router.route("/getrecentactivity").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getRecentActivity);
-router.route("/getuserperformance/:userId").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getUserPerformance);
-router.route("/getalluserperformances").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getAllUserPerformances);
-router.route("/priority-issues").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getPriorityIssues);
-router.route("/total-issues-by-user").get(isAuthenticated, verifyToken, getIssuesCreatedByUser);
-router.route("/get-weekly-review").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getWeeklyReview)
-router.route("/get-department-wise-analyis").get(isAuthenticated, verifyToken, authorize(["admin", "employee"]), getDepartmentWiseAnalysis)
+router.route("/addissue").post( verifyToken, validateLawIssueData, handleValidationErrors, authorize(["admin", "employee"]), addIssue)
+router.route("/updateissue/:issueId").put( verifyToken, validateLawIssueData, handleValidationErrors, authorize(["admin", "employee"]), updateIssue);
+router.route("/getallissues").get( verifyToken, authorize(["admin", "employee"]), getAllIssues);
+router.route("/addcomment/:issueId").post( verifyToken, validateLawIssueDataComment, handleValidationErrors, authorize(["admin", "employee"]), addComment);
+router.route("/getcomment/:issueId").get( verifyToken, authorize(["admin", "employee"]), getComments);
+router.route("/updatecomment/:issueId/:commentId").put( verifyToken, validateLawIssueDataComment, handleValidationErrors, authorize(["admin", "employee"]), updateComment);
+router.route("/getissuebyid/:issueId").get( verifyToken, authorize(["admin", "employee"]), getIssueById);
+router.route("/getissuesbycreatedby/:userId").get( verifyToken, authorize(["admin", "employee"]), getIssuesByCreatedBy);
+router.route("/getissuesbylogin").get( verifyToken, authorize(["admin", "employee"]), getIssuesByLogin);
+router.route("/deleteissue/:issueId").delete( verifyToken, authorize(["admin", "employee"]), deleteIssue);
+router.route("/deletecomment/:issueId/:commentId").delete( verifyToken, authorize(["admin", "employee"]), deleteComment);
+router.route("/exportlawissues").get( verifyToken, authorize(["admin", "employee"]), exportLawIssues);
+router.route("/getdashboardsummery").get( verifyToken, authorize(["admin", "employee"]), getDashboardSummary);
+router.route("/getuserdashboardsummary").get( verifyToken, authorize(["admin", "employee"]), getUserDashboardSummary);
+router.route("/changeissuestatus").put( verifyToken, changeIssueStatusValidation, handleValidationErrors, authorize(["admin", "employee"]), changeIssueStatus);
+router.route("/getrecentactivities").get( verifyToken, authorize(["admin", "employee"]), getRecentActivities);
+router.route("/getrecentactivity").get( verifyToken, authorize(["admin", "employee"]), getRecentActivity);
+router.route("/getuserperformance/:userId").get( verifyToken, authorize(["admin", "employee"]), getUserPerformance);
+router.route("/getalluserperformances").get( verifyToken, authorize(["admin", "employee"]), getAllUserPerformances);
+router.route("/priority-issues").get( verifyToken, authorize(["admin", "employee"]), getPriorityIssues);
+router.route("/total-issues-by-user").get( verifyToken, getIssuesCreatedByUser);
+router.route("/get-weekly-review").get( verifyToken, authorize(["admin", "employee"]), getWeeklyReview)
+router.route("/get-department-wise-analyis").get( verifyToken, authorize(["admin", "employee"]), getDepartmentWiseAnalysis)
 module.exports = router;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -28,17 +28,17 @@ const {
 } = require("../middlewares/validation")
 const router = express.Router();
 router.route("/register").post(validateUserRegistration, handleValidationErrors, Register);
-router.route("/registeremployee").post(isAuthenticated, verifyToken, authorize(['admin']), validateEmployeeRegistration, handleValidationErrors, RegisterEmployee);
+router.route("/registeremployee").post( verifyToken, authorize(['admin']), validateEmployeeRegistration, handleValidationErrors, RegisterEmployee);
 router.route("/resetpassword").post(validateResetPassowrd, handleValidationErrors, resetPassword);
-router.route("/changepassword").put(isAuthenticated, verifyToken, validateOldPasswordChange, handleValidationErrors, changePassword);
-router.route("/changenewuserpassword").put(isAuthenticated, verifyToken, validateNewUserPassword, handleValidationErrors, changeNewUserPassword);
+router.route("/changepassword").put( verifyToken, validateOldPasswordChange, handleValidationErrors, changePassword);
+router.route("/changenewuserpassword").put( verifyToken, validateNewUserPassword, handleValidationErrors, changeNewUserPassword);
 router.route("/resetpassword/:token").put(validateChangePassword, handleValidationErrors, resetPasswordToken);
 router.route("/login").post(validateUserLogin, handleValidationErrors, login);
-router.route("/profile").get(isAuthenticated, verifyToken, authorize(['employee', 'admin']), profile);
+router.route("/profile").get( verifyToken, authorize(['employee', 'admin']), profile);
 router.route("/logout").get(logout);
-router.route("/all-users").get(isAuthenticated, verifyToken, authorize(['admin']), allUsers);
-router.route("/deactivated-users").get(isAuthenticated, verifyToken, authorize(['admin']), deactivatedAccounts);
-router.route("/change-account-status").put(isAuthenticated, verifyToken, authorize(['admin']), validateStatusChange, handleValidationErrors, changeUserStatus);
+router.route("/all-users").get( verifyToken, authorize(['admin']), allUsers);
+router.route("/deactivated-users").get( verifyToken, authorize(['admin']), deactivatedAccounts);
+router.route("/change-account-status").put( verifyToken, authorize(['admin']), validateStatusChange, handleValidationErrors, changeUserStatus);
 
 module.exports = router;
 
